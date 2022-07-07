5c3cdfbdd158d7df3cf8b38df21b695c4463b30e
tbsklg <tbs.klg@gmail.com>
2022-07-07 13:30:07 +0200

    Solve 559ac78160f0be07c200005a: Name Shuffler

---
+++ test/name_shuffler/nameShuffler.spec.js
@@ -0,0 +1,16 @@
+const chai = require('chai')
+const expect = chai.expect
+
+const Test = {
+  assertEquals (actual, expected) {
+    expect(actual).to.equal(expected)
+  }
+}
+
+const nameShuffler = require('../../src/name_shuffler/nameShuffler')
+
+describe("Tests", () => {
+    it("test", () => {
+        Test.assertEquals(nameShuffler('john McClane'), 'McClane john')
+    })
+})

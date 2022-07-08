5c818c1e5af6e2f2eb7a1bb2f0f390c7662e6154
tbsklg <tbs.klg@gmail.com>
2022-07-08 10:12:13 +0200

    Solve 5202ef17a402dd033c000009: Title Case

---
+++ src/title_case/titleCase.js
@@ -0,0 +1,24 @@
+function titleCase(title, minorWords) {
+    const [head, ...tail] = title.split(" ");
+
+    return [
+        capitalize(head),
+        ...tail.map((word) => {
+            if (
+                !minorWords ||
+                !minorWords
+                    .split(" ")
+                    .some((minorWord) => minorWord.toLowerCase() === word.toLowerCase())
+            ) {
+                return capitalize(word);
+            }
+            return word.toLowerCase();
+        }),
+    ].join(" ");
+}
+
+function capitalize(word) {
+    return word.toLowerCase().replace(/\w/, (first) => first.toUpperCase());
+}
+
+module.exports = titleCase;

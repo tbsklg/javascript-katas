bf783cde9707688d7fab3dfbbfb527e85513a0ce
tbsklg <tbs.klg@gmail.com>
2022-07-11 09:00:57 +0200

    Solve 54a91a4883a7de5d7800009c: String incrementer

---
+++ src/increment_string/incrementString.js
@@ -0,0 +1,6 @@
+function incrementString(strng) {
+    return strng.replace(/[0-8]?9*$/, n => n ? parseInt(n) + 1 : 1)
+    // return strng.replace(/[0-8]?9*$/, n => n ? + n + 1 : 1)
+}
+
+module.exports = incrementString

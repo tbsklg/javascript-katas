68f693c06187c3485a7b918b8f7c1698b1b4f1f3
tbsklg <tbs.klg@gmail.com>
2022-07-11 18:39:03 +0200

    Solve 5270d0d18625160ada0000e4: Greed is Good

---
+++ test/score/score.spec.js
@@ -0,0 +1,24 @@
+const chai = require('chai')
+const expect = chai.expect
+
+const Test = {
+    expect(actual, expected) {
+        expect(actual).to.equal(expected)
+    }
+}
+
+const score = require('../../src/score/score')
+
+describe("Scorer Function", function () {
+    it("should value this as worthless", function () {
+        Test.expect(score([2, 3, 4, 6, 2]), 0);
+    });
+
+    it("should value this triplet correctly", function () {
+        Test.expect(score([4, 4, 4, 3, 3]), 400);
+    });
+
+    it("should value this mixed set correctly", function () {
+        Test.expect(score([2, 4, 4, 5, 4]), 450);
+    });
+});

c47677fb78bf81b4df5c25241865f1fed4878eda
tbsklg <tbs.klg@gmail.com>
2022-07-19 13:55:11 +0200

    Update 52e1476c8147a7547a000811: Regex Password Validation

--- src/password_validation/regex.js
+++ src/password_validation/regex.js
@@ -1,4 +1,11 @@
-// assign your RegExp to REGEXP
+/*
+Regex for validating a password which meets the following criteria:
+- at least six characters long
+- contains a lowercase letter
+- contains an uppercase letter
+- contains a digit
+- contains only alphanumeric characters (note that '_' is not alphanumeric)
+ */
 const REGEXP = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{6,}$/
 
 module.exports.REGEXP = REGEXP

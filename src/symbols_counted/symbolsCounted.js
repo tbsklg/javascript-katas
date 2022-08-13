ac2ee374919eab1e9fa7223f4c89284008bd88f1
tbsklg <tbs.klg@gmail.com>
2022-08-13 08:48:17 +0200

    Solve 59ddeeb2fc3c49186f00009c: Symbols counted

---
+++ src/symbols_counted/symbolsCounted.js
@@ -0,0 +1,13 @@
+const transform = s => {
+    if (s.length === 0) return ""
+
+    const [head] = s.split('')
+    const count = s.split('').filter(x => x === head).length
+    const next = s.split('').filter(x => x !== head).join('')
+
+    if (count > 1) return head + count + transform(next)
+
+    return head + transform(next)
+};
+
+module.exports = transform

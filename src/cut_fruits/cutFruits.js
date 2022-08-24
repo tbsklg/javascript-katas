fb89ccecf48fedc26aae4b0da34c3eb6ae64468b
tbsklg <tbs.klg@gmail.com>
2022-08-24 09:21:22 +0200

    Solve 57d60363a65454701d000e11: I guess this is a 7kyu kata #6: Fruit Ninja I

---
+++ src/cut_fruits/cutFruits.js
@@ -0,0 +1,28 @@
+function cutFruits(fruits) {
+    return fruits
+        .map(fruit => {
+            if (isFruit(fruit)) return cut(fruit)
+
+            return [fruit]
+        })
+        .reduce((acc, curr) => [...acc, ...curr])
+}
+
+function cut(fruit) {
+    const isOdd = fruit.length % 2 !== 0
+    const half = fruit.length / 2
+
+    if (isOdd) {
+        return [fruit.slice(0, half + 1), fruit.slice(half + 1)]
+    }
+
+    return [fruit.slice(0, half), fruit.slice(half)]
+}
+
+const fruits = ["apple", "pear", "banana"]
+
+function isFruit(f) {
+    return fruits.find(x => x === f)
+}
+
+module.exports = cutFruits

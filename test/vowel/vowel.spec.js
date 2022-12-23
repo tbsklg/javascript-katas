64341216092a53db8c0c4d322e68dd5a7f6de3fc
Tobias Klug <tbs.klg@gmail.com>
2022-12-23 08:10:10 +0100

    Solve 54ff3102c1bad923760001f3: Vowel Count

---
+++ test/vowel/vowel.spec.js
@@ -0,0 +1,9 @@
+const {assert} = require("chai");
+
+const getCount = require('../../src/vowel/vowel');
+
+describe("Vowels Count Tests",function(){
+  it("should return 5 for 'abracadabra'",function(){
+    assert.strictEqual(getCount("abracadabra"), 5) ;
+  });
+});

cae6306d6f6d570c1211a53b72ce9f5b71166210
Tobias Klug <tbs.klg@gmail.com>
2023-01-05 08:10:10 +0100

    Solve 55a5bfaa756cfede78000026: Super Duper Easy

---
+++ src/problem/problem.js
@@ -0,0 +1,9 @@
+function problem (x) {
+  if (typeof x !== 'number') {
+    return 'Error'
+  }
+
+  return 50 * x + 6
+}
+
+module.exports = problem
---
+++ test/problem/problem.spec.js
@@ -0,0 +1,21 @@
+const chai = require("chai");
+
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+const problem = require('../../src/problem/problem');
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(problem("hello"), "Error");
+    assert.strictEqual(problem(1), 56);
+    assert.strictEqual(problem(5), 256);
+    assert.strictEqual(problem(0), 6);
+    assert.strictEqual(problem(1.2), 66);
+    assert.strictEqual(problem(3), 156);
+    assert.strictEqual(problem("RyanIsCool"), "Error");
+    assert.strictEqual(problem(-3), -144);
+    assert.strictEqual(problem(""), "Error");
+    assert.strictEqual(problem(0.03), 7.5);
+  })
+})

742576c9f82cd27914ef095545311deab34eec44
Tobias Klug <tbs.klg@gmail.com>
2023-01-23 08:04:36 +0100

    Solve 5502c9e7b3216ec63c0001aa: Categorize New Member

---
+++ src/open_or_senior/openOrSenior.js
@@ -0,0 +1,11 @@
+function openOrSenior (data) {
+  return data.map(([age, handicap]) => {
+    if (age > 54 && handicap > 6) {
+      return 'Senior'
+    }
+
+    return 'Open'
+  })
+}
+
+module.exports = openOrSenior

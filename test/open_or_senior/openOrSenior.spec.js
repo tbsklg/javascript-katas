7a57a48d026368ff05fb391959301ed20d1d4b4d
Tobias Klug <tbs.klg@gmail.com>
2023-01-24 20:18:16 +0100

    Solve 5245a9138ca049e9a10007b8: Adjacent repeated words in a string

--- test/open_or_senior/openOrSenior.spec.js
+++ test/open_or_senior/openOrSenior.spec.js
@@ -1,13 +1,13 @@
-const chai = require("chai");
-const assert = chai.assert;
-chai.config.truncateThreshold=0;
+const chai = require('chai')
+const assert = chai.assert
+chai.config.truncateThreshold = 0
 
 const openOrSenior = require('../../src/open_or_senior/openOrSenior')
 
-describe("Basic tests", () => {
-  it("Fixed tests", () => {
-    assert.deepEqual(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]),['Open', 'Senior', 'Open', 'Senior'])
-    assert.deepEqual(openOrSenior([[3, 12],[55,1],[91, -2],[53, 23]]),['Open', 'Open', 'Open', 'Open'])
-    assert.deepEqual(openOrSenior([[59, 12],[55,-1],[12, -2],[12, 12]]),['Senior', 'Open', 'Open', 'Open'])
-  });
-});
+describe('Basic tests', () => {
+  it('Fixed tests', () => {
+    assert.deepEqual(openOrSenior([[45, 12], [55, 21], [19, -2], [104, 20]]), ['Open', 'Senior', 'Open', 'Senior'])
+    assert.deepEqual(openOrSenior([[3, 12], [55, 1], [91, -2], [53, 23]]), ['Open', 'Open', 'Open', 'Open'])
+    assert.deepEqual(openOrSenior([[59, 12], [55, -1], [12, -2], [12, 12]]), ['Senior', 'Open', 'Open', 'Open'])
+  })
+})

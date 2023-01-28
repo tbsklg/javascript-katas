fca003efe6249eab9f87c28861b083b2598deee9
Tobias Klug <tbs.klg@gmail.com>
2023-01-28 07:10:10 +0100

    Solve 54b42f9314d9229fd6000d9c: Duplicate Encoder

---
+++ src/duplicate_encode/duplicateEncode.js
@@ -0,0 +1,12 @@
+function duplicateEncode (word) {
+  const chars = word.toLowerCase().split('')
+  const charCount = chars.reduce((acc, curr) =>
+    ({ ...acc, [curr]: (acc[curr] || 0) + 1 }), {})
+
+  return chars.map(char => {
+    const count = charCount[char] || 0
+    return count === 1 ? '(' : ')'
+  }).join('')
+}
+
+module.exports = duplicateEncode
--- test/ count_adjacent_pairs/countAdjacentPairs.spec.js
+++ test/ count_adjacent_pairs/countAdjacentPairs.spec.js
@@ -9,12 +9,12 @@ const Test = {
 
 const countAdjacentPairs = require('../../src/count_adjacent_pairs/countAdjacentPairs')
 
-describe("Tests", () => {
-  it("test", () => {
+describe('Tests', () => {
+  it('test', () => {
     Test.assertEquals(countAdjacentPairs(''), 0, 'An empty string should return 0.')
     Test.assertEquals(countAdjacentPairs('orange Orange kiwi pineapple apple'), 1, "Case should be ignored. countAdjacentPairs('orange Orange kiwi pineapple apple')")
     Test.assertEquals(countAdjacentPairs('banana banana banana'), 1, "Once a word has been paired, it is ignored. countAdjacentPairs('banana banana banana')")
     Test.assertEquals(countAdjacentPairs('banana banana banana terracotta banana terracotta terracotta pie!'), 2, "Once a word has been paired, it is ignored. Grab all pairs. countAdjacentPairs('banana banana banana terracotta banana terracotta terracotta pie!')")
     Test.assertEquals(countAdjacentPairs('pineapple apple'), 0, "A pineapple is not an apple. countAdjacentPairs('pineapple apple')")
-  });
-});
+  })
+})

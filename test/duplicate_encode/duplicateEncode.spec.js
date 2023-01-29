4fa4d7dee350f41631c9056ac6984544174b0439
Tobias Klug <tbs.klg@gmail.com>
2023-01-29 07:10:10 +0100

    Fix linting error

--- test/duplicate_encode/duplicateEncode.spec.js
+++ test/duplicate_encode/duplicateEncode.spec.js
@@ -1,14 +1,14 @@
-const chai = require("chai");
-const assert = chai.assert;
-chai.config.truncateThreshold=0;
+const chai = require('chai')
+const assert = chai.assert
+chai.config.truncateThreshold = 0
 
 const duplicateEncode = require('../../src/duplicate_encode/duplicateEncode')
 
-describe("Duplicate Encoder", () => {
-  it("Testing for fixed tests", () => {
-    assert.strictEqual(duplicateEncode("recede"),"()()()");
-    assert.strictEqual(duplicateEncode("din"),"(((");
-    assert.strictEqual(duplicateEncode("Success"),")())())","should ignore case");
-    assert.strictEqual(duplicateEncode("(( @"),"))((");
-  });
-});
+describe('Duplicate Encoder', () => {
+  it('Testing for fixed tests', () => {
+    assert.strictEqual(duplicateEncode('recede'), '()()()')
+    assert.strictEqual(duplicateEncode('din'), '(((')
+    assert.strictEqual(duplicateEncode('Success'), ')())())', 'should ignore case')
+    assert.strictEqual(duplicateEncode('(( @'), '))((')
+  })
+})

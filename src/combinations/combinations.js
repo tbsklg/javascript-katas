6a6439cde169b0649bb5385dc9b3943585ad843b
Tobias Klug <tbs.klg@gmail.com>
2023-01-30 07:10:10 +0100

    Solve 59e66e48fc3c499ec5000103: Array combinations

---
+++ src/combinations/combinations.js
@@ -0,0 +1,8 @@
+function solve (arrays) {
+  return arrays
+    .map(arr => new Set(arr))
+    .map(arr => arr.size)
+    .reduce((acc, curr) => acc * curr, 1)
+}
+
+module.exports = solve
---
+++ test/combinations/combinations.spec.js
@@ -0,0 +1,19 @@
+const chai = require('chai')
+const expect = chai.expect
+
+const solve = require('../../src/combinations/combinations')
+
+const Test = {
+  assertEquals (actual, expected) {
+    expect(actual).to.equal(expected)
+  }
+}
+
+describe('Basic tests', function () {
+  it('should calculate array combinations', () => {
+    Test.assertEquals(solve([[1, 2], [4], [5, 6]]), 4)
+    Test.assertEquals(solve([[1, 2], [4, 4], [5, 6, 6]]), 4)
+    Test.assertEquals(solve([[1, 2], [3, 4], [5, 6]]), 8)
+    Test.assertEquals(solve([[1, 2, 3], [3, 4, 6, 6, 7], [8, 9, 10, 12, 5, 6]]), 72)
+  })
+})

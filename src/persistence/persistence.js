647721915afa771bcdb411e9d18d51ef1f3c4945
Tobias Klug <tbs.klg@gmail.com>
2023-02-02 06:10:10 +0100

    Solve 55bf01e5a717a0d57e0000ec: Persistent Bugger.

---
+++ src/persistence/persistence.js
@@ -0,0 +1,14 @@
+function persistence (num) {
+  const digits = digitsOf(num)
+  if (digits.length < 2) return 0
+
+  const nextNumber = digits.reduce((acc, curr) => acc * curr, 1)
+  return 1 + persistence(nextNumber)
+}
+
+const digitsOf = (num) => {
+  if (num === 0) return []
+  return [...digitsOf(Math.floor(num / 10)), num % 10]
+}
+
+module.exports = persistence
---
+++ test/persistence/persistence.spec.js
@@ -0,0 +1,14 @@
+const chai = require('chai')
+const assert = chai.assert
+chai.config.truncateThreshold = 0
+
+const persistence = require('../../src/persistence/persistence')
+
+describe('Persistent Bugger.', () => {
+  it('Fixed tests', () => {
+    // assert.strictEqual(persistence(39),3);
+    // assert.strictEqual(persistence(4),0);
+    assert.strictEqual(persistence(25), 2)
+    assert.strictEqual(persistence(999), 4)
+  })
+})

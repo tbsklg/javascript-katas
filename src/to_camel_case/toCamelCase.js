6db5d8b53305812530887910bd7c6e7e0d0441a9
Tobias Klug <tbs.klg@gmail.com>
2023-02-05 06:10:10 +0100

    Solve 517abf86da9663f1d2000003: Convert string to camel case

---
+++ src/to_camel_case/toCamelCase.js
@@ -0,0 +1,6 @@
+function toCamelCase (str) {
+  const [head, ...tail] = str.split(/[_-]/)
+  return [head, ...tail.map(([first, ...rest]) => [first.toUpperCase(), ...rest].join(''))].join('')
+}
+
+module.exports = toCamelCase
---
+++ test/to_camel_case/toCamelCase.spec.js
@@ -0,0 +1,12 @@
+const { assert } = require('chai')
+
+const toCamelCase = require('../../src/to_camel_case/toCamelCase')
+
+describe('Tests', () => {
+  it('test', () => {
+    // assert.strictEqual(toCamelCase(''), '', "An empty string was provided but not returned")
+    assert.strictEqual(toCamelCase('the_stealth_warrior'), 'theStealthWarrior', "toCamelCase('the_stealth_warrior') did not return correct value")
+    assert.strictEqual(toCamelCase('The-Stealth-Warrior'), 'TheStealthWarrior', "toCamelCase('The-Stealth-Warrior') did not return correct value")
+    assert.strictEqual(toCamelCase('A-B-C'), 'ABC', "toCamelCase('A-B-C') did not return correct value")
+  })
+})

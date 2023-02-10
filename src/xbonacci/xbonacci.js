c579d885146b73393a7cbe7b799adb566b8660bf
Tobias Klug <tbs.klg@gmail.com>
2023-02-10 06:10:10 +0100

    Solve 556e0fccc392c527f20000c5: Fibonacci, Tribonacci and friends

---
+++ src/xbonacci/xbonacci.js
@@ -0,0 +1,14 @@
+function Xbonacci (signature, n) {
+  if (n < signature.length) return signature.slice(0, n)
+
+  return Array
+    .from({ length: n - signature.length }, (_, v) => v)
+    .reduce((acc, curr) =>
+      [
+        ...acc,
+        acc.slice(curr).reduce((acc, curr) => acc + curr, 0)
+      ], signature
+    )
+}
+
+module.exports = Xbonacci
---
+++ test/xbonacci/xbonacci.spec.js
@@ -0,0 +1,17 @@
+const chai = require('chai')
+
+const Test = {
+  assertSimilar (actual, expected) {
+    chai.expect(actual).to.include.members(expected)
+  }
+}
+
+const Xbonacci = require('../../src/xbonacci/xbonacci')
+
+describe('Basic tests', function () {
+  Test.assertSimilar(Xbonacci([0, 1], 10), [0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
+  Test.assertSimilar(Xbonacci([1, 1], 10), [1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+  Test.assertSimilar(Xbonacci([0, 0, 0, 0, 1], 10), [0, 0, 0, 0, 1, 1, 2, 4, 8, 16])
+  Test.assertSimilar(Xbonacci([1, 0, 0, 0, 0, 0, 1], 10), [1, 0, 0, 0, 0, 0, 1, 2, 3, 6])
+  Test.assertSimilar(Xbonacci([1, 0, 0, 0, 0, 0, 0, 0, 0, 0], 20), [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 4, 8, 16, 32, 64, 128, 256])
+})

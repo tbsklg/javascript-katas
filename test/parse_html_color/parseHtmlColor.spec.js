fcda80fcc94c631317e82c1255fbd503f98c51b9
Tobias Klug <tbs.klg@gmail.com>
2023-02-12 14:13:17 +0100

    Solve 58b57ae2724e3c63df000006: Parse HTML/CSS Colors

---
+++ test/parse_html_color/parseHtmlColor.spec.js
@@ -0,0 +1,13 @@
+const { expect } = require('chai')
+
+const parseHTMLColor = require('../../src/parse_html_color/parseHtmlColor')
+
+const assertColorEquals = (current, expected) => expect(current).to.eql(expected)
+
+describe('Example tests', () => {
+  it('should parse example colors', () => {
+    assertColorEquals(parseHTMLColor('#80FFA0'), { r: 128, g: 255, b: 160 })
+    assertColorEquals(parseHTMLColor('#3B7'), { r: 51, g: 187, b: 119 })
+    assertColorEquals(parseHTMLColor('LimeGreen'), { r: 50, g: 205, b: 50 })
+  })
+})

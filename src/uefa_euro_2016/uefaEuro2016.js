48bc37eb60772a74128895e2909d208d2122bd6e
Tobias Klug <tbs.klg@gmail.com>
2023-02-15 06:10:10 +0100

    Solve 57613fb1033d766171000d60: UEFA EURO 2016

---
+++ src/uefa_euro_2016/uefaEuro2016.js
@@ -0,0 +1,11 @@
+function uefaEuro2016 ([homeTeam, awayTeam], [homeScores, awayScores]) {
+  const match = `At match ${homeTeam} - ${awayTeam}`
+  const isDraw = homeScores === awayScores
+  if (isDraw) return `${match}, teams played draw.`
+
+  const winner = homeScores > awayScores ? homeTeam : awayTeam
+
+  return `${match}, ${winner} won!`
+}
+
+module.exports = uefaEuro2016
---
+++ test/uefa_euro_2016/uefaEuro2016.spec.js
@@ -0,0 +1,13 @@
+const chai = require('chai')
+const assert = chai.assert
+chai.config.truncateThreshold = 0
+
+const uefaEuro2016 = require('../../src/uefa_euro_2016/uefaEuro2016')
+
+describe('Basic tests', () => {
+  it('Testing for fixed tests', () => {
+    assert.strictEqual(uefaEuro2016(['Germany', 'Ukraine'], [2, 0]), 'At match Germany - Ukraine, Germany won!')
+    assert.strictEqual(uefaEuro2016(['Belgium', 'Italy'], [0, 2]), 'At match Belgium - Italy, Italy won!')
+    assert.strictEqual(uefaEuro2016(['Portugal', 'Iceland'], [1, 1]), 'At match Portugal - Iceland, teams played draw.')
+  })
+})

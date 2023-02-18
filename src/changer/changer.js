99cc75f6f3861797b02bdcf91c127e67afe5f808
Tobias Klug <tbs.klg@gmail.com>
2023-02-18 06:10:10 +0100

    Solve 58039f8efca342e4f0000023: Change it up

---
+++ src/changer/changer.js
@@ -0,0 +1,30 @@
+function changer (str) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+  const shift = (x) => {
+    if (alphabet.indexOf(x) === -1) return x
+
+    if (x === 'z') return 'a'
+    return alphabet[alphabet.indexOf(x) + 1]
+  }
+
+  const isVowel = (x) => 'aeiou'.indexOf(x) !== -1
+
+  const words = (x) => x.split(' ')
+
+  const unwords = (x) => x.join(' ')
+
+  const capitalizeIfVowel = (x) => isVowel(x) ? x.toUpperCase() : x
+
+  return unwords(
+    words(str)
+      .map(x => x.split('')
+        .map(x => x.toLowerCase())
+        .map(x => shift(x))
+        .map(x => capitalizeIfVowel(x))
+        .join('')
+      )
+  )
+}
+
+module.exports = changer

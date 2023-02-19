8ca8c8c724b5d9d89dd89e508870ce2cbbce41a6
Tobias Klug <tbs.klg@gmail.com>
2023-02-19 06:10:10 +0100

    Solve 526571aae218b8ee490006f4: Bit Counting

---
+++ src/bit_counting/bitCounting.js
@@ -0,0 +1,11 @@
+const countBits = function (n) {
+  const toBinary = (x) => {
+    if (x === 0) return []
+
+    return [...toBinary(Math.floor(x / 2)), x % 2]
+  }
+
+  return toBinary(n).filter(x => x === 1).length
+}
+
+module.exports = countBits

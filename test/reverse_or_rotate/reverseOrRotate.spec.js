6fc841497b493e858433d412cace64d5e7764872
Tobias Klug <tbs.klg@gmail.com>
2023-02-21 06:45:11 +0100

    Solve 56b5afb4ed1f6d5fb0000991: Reverse or rotate?

---
+++ test/reverse_or_rotate/reverseOrRotate.spec.js
@@ -0,0 +1,18 @@
+const chai = require('chai')
+const assert = chai.assert
+
+function testing (actual, expected) {
+  assert.strictEqual(actual, expected)
+}
+
+const revrot = require('../../src/reverse_or_rotate/reverseOrRotate')
+
+describe('revrot', function () {
+  it('Basic tests', function () {
+    testing(revrot('1234', 0), '')
+    testing(revrot('', 0), '')
+    testing(revrot('1234', 5), '')
+    const s = '733049910872815764'
+    testing(revrot(s, 5), '330479108928157')
+  })
+})

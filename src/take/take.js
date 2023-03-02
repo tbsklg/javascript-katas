572cfc22837b7e119df83fb999440f0b9a921746
Tobias Klug <tbs.klg@gmail.com>
2023-03-02 06:10:10 +0100

    Solve 545afd0761aa4c3055001386: Enumerable Magic #25 - Take the First N Elements

---
+++ src/take/take.js
@@ -0,0 +1,5 @@
+function take(arr, n) {
+  return arr.slice(0, n)
+}
+
+module.exports = take
---
+++ test/take/take.spec.js
@@ -0,0 +1,16 @@
+const chai = require('chai')
+const expect = chai.expect
+
+const Test = {
+  assertDeepEquals (actual, expected) {
+    expect(actual).to.deep.equal(expected)
+  }
+}
+
+const take = require('../../src/take/take')
+
+describe("Sample Tests", function(){
+  it("should work for sample tests", function(){
+    Test.assertDeepEquals(take([0, 1, 2, 3, 5, 8, 13], 3), [0, 1, 2], "should return the first 3 items");
+  });
+});

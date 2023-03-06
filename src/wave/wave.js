96edce35f6520e15d1d09aae0692397b1ff93fca
Tobias Klug <tbs.klg@gmail.com>
2023-03-06 06:10:10 +0100

    Solve 58f5c63f1e26ecda7e000029: Mexican Wave

---
+++ src/wave/wave.js
@@ -0,0 +1,20 @@
+function wave (str) {
+  const createWave = (str, [i, ...is]) => {
+    if (i === undefined) return []
+
+    const letters = str.split('')
+    const letter = letters[i]
+
+    if (letter === ' ') return createWave(str, is)
+
+    const preLetter = letters.slice(0, i).join('')
+    const postLetter = letters.slice(i + 1).join('')
+    const newStr = preLetter + letter.toUpperCase() + postLetter
+
+    return [newStr, ...createWave(str, is)]
+  }
+
+  return createWave(str, Array.from({ length: str.length }, (_, i) => i))
+}
+
+module.exports = wave
---
+++ test/wave/wave.spec.js
@@ -0,0 +1,24 @@
+const chai = require('chai')
+const { assert } = chai
+chai.config.truncateThreshold = 0
+
+const wave = require('../../src/wave/wave')
+
+describe('Testing...', function () {
+  it('Basic tests', () => {
+    let result = ['Hello', 'hEllo', 'heLlo', 'helLo', 'hellO']
+    assert.deepEqual(wave('hello'), result, 'Should return: \'' + result + '\'')
+
+    result = ['Codewars', 'cOdewars', 'coDewars', 'codEwars', 'codeWars', 'codewArs', 'codewaRs', 'codewarS']
+    assert.deepEqual(wave('codewars'), result, 'Should return: \'' + result + '\'')
+
+    result = []
+    assert.deepEqual(wave(''), result, 'Should return: \'' + result + '\'')
+
+    result = ['Two words', 'tWo words', 'twO words', 'two Words', 'two wOrds', 'two woRds', 'two worDs', 'two wordS']
+    assert.deepEqual(wave('two words'), result, 'Should return: \'' + result + '\'')
+
+    result = [' Gap ', ' gAp ', ' gaP ']
+    assert.deepEqual(wave(' gap '), result, 'Should return: \'' + result + '\'')
+  })
+})

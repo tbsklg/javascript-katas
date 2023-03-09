9258cb2d9c7d1c37e1466ade523eeb6c56200d7a
Tobias Klug <tbs.klg@gmail.com>
2023-03-09 06:10:10 +0100

    Solve 51e0007c1f9378fa810002a9: Make the Deadfish Swim

---
+++ src/parse/parse.js
@@ -0,0 +1,19 @@
+function parse (raw) {
+  const performParsing = ([x, ...xs], currentResult = 0) => {
+    const increment = (i) => i + 1
+    const decrement = (i) => i - 1
+    const square = (i) => Math.pow(i, 2)
+
+    if (x === undefined) return []
+    if (x === 'i') return performParsing(xs, increment(currentResult))
+    if (x === 'd') return performParsing(xs, decrement(currentResult))
+    if (x === 's') return performParsing(xs, square(currentResult))
+    if (x === 'o') return [currentResult, ...performParsing(xs, currentResult)]
+
+    return performParsing(xs, currentResult)
+  }
+
+  return performParsing(raw.split(''))
+}
+
+module.exports = parse
---
+++ test/parse/parse.spec.js
@@ -0,0 +1,17 @@
+const chai = require('chai')
+const expect = chai.expect
+
+const Test = {
+  assertDeepEquals (actual, expected) {
+    expect(actual).to.deep.equal(expected)
+  }
+}
+
+const parse = require('../../src/parse/parse')
+
+describe('Tests', () => {
+  it('test', () => {
+    Test.assertDeepEquals(parse('iiisdoso'), [8, 64])
+    Test.assertDeepEquals(parse('iiisxxxdoso'), [8, 64])
+  })
+})

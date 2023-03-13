ada158f3ee68678d48ee58293d1bbe134ea43960
Tobias Klug <tbs.klg@gmail.com>
2023-03-13 21:10:29 +0100

    Solve 5862fb364f7ab46270000078: Basic Encryption

---
+++ src/encrypt/encrypt.js
@@ -0,0 +1,13 @@
+function encrypt (text, rule) {
+  if (text === '') return ''
+
+  const shiftChar = c => {
+    const shift = c.charCodeAt(0) + rule
+    const shift255 = shift > 255 ? shift % 256 : shift
+    return String.fromCharCode(shift255)
+  }
+
+  return text.split('').map(c => shiftChar(c)).join('')
+}
+
+module.exports = encrypt

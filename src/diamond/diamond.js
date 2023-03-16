2ebfbcbfe2eb2c47033cd3b7b618c057f1fa52ce
Tobias Klug <tbs.klg@gmail.com>
2023-03-16 16:40:53 +0100

    Solve 5503013e34137eeeaa001648: Give me a Diamond

---
+++ src/diamond/diamond.js
@@ -0,0 +1,18 @@
+function diamond (n) {
+  if (n % 2 === 0 || n < 1) return null
+
+  if (n === 1) return '*\n'
+
+  const draw = (i) => {
+    if (i === n) return '*'.repeat(i).concat('\n')
+
+    const stars = ('*'.repeat(i)).concat('\n')
+    const spaces = ' '.repeat((n - i) / 2)
+    const part = spaces.concat(stars)
+    return part.concat(...draw(i + 2)).concat(part)
+  }
+
+  return draw(1)
+}
+
+module.exports = diamond

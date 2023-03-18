55fc1ab64cefae063c8ba2958dd71575f051dc05
Tobias Klug <tbs.klg@gmail.com>
2023-03-18 09:41:26 +0100

    Extend 58de819eb76cf778fe00005c: Numerical Palindrome #2

--- src/palindrome/palindrome.js
+++ src/palindrome/palindrome.js
@@ -14,4 +14,10 @@ function palindrome (num) {
   return containsPalindrome(numAsString)
 }
 
+// regex for the rescue: '(\d)\d?\1'
+// 1st Capturing Group (\d)
+// \d matches a digit (equivalent to [0-9])
+// \d matches a digit (equivalent to [0-9])
+// ? matches the previous token between zero and one times, as many times as possible, giving back as needed (greedy)
+// \1 matches the same text as most recently matched by the 1st capturing group
 module.exports = palindrome

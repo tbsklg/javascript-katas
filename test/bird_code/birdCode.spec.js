0ffd296e91feec1d589587acf1bf4d395da664b0
Tobias Klug <tbs.klg@gmail.com>
2023-03-21 07:56:32 +0100

    Solve 5a580064e6be38fd34000147: Create Four Letter Birding Codes from Bird Names

---
+++ test/bird_code/birdCode.spec.js
@@ -0,0 +1,14 @@
+const chai = require('chai')
+const assert = chai.assert
+
+const Test = {
+  assertDeepEquals: (actual, expected) => assert.deepEqual(actual, expected)
+}
+
+const birdCode = require('../../src/bird_code/birdCode')
+
+describe('Tests', () => {
+  it('test', () => {
+    Test.assertDeepEquals(birdCode(['Common Tern', 'Black-Capped Chickadee']), ['COTE', 'BCCH'])
+  })
+})

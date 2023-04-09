652b984180829c0a3b0215b989d07aab7e24cf73
Tobias Klug <tbs.klg@gmail.com>
2023-04-09 10:47:27 +0200

    Solve 5839edaa6754d6fec10000a2: Find the missing letter

---
+++ src/missing_letter/findMissingLetter.js
@@ -0,0 +1,7 @@
+function findMissingLetter ([x, y, ...xs]) {
+  if (y.charCodeAt(0) - x.charCodeAt(0) === 1) return findMissingLetter([y, ...xs])
+
+  return String.fromCharCode(x.charCodeAt(0) + 1)
+}
+
+module.exports = findMissingLetter
---
+++ test/find_missing_letter/findMissingLetter.spec.js
@@ -0,0 +1,15 @@
+const chai = require('chai')
+const assert = chai.assert
+
+const Test = {
+  assertEquals: assert.strictEqual
+}
+
+const findMissingLetter = require('../../src/missing_letter/findMissingLetter')
+
+describe('KataTests', function () {
+  it('exampleTests', function () {
+    Test.assertEquals(findMissingLetter(['a', 'b', 'c', 'd', 'f']), 'e')
+    Test.assertEquals(findMissingLetter(['O', 'Q', 'R', 'S']), 'P')
+  })
+})

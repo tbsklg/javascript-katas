e1d2c0c16b49d2f844e08f5d077ebb5aeec0f43e
Tobias Klug <tbs.klg@gmail.com>
2023-06-22 05:58:11 +0200

    Solve 5a946d9fba1bb5135100007c: Transform To Prime

---
+++ src/minimum_number/minimumNumber.js
@@ -0,0 +1,18 @@
+function minimumNumber (numbers) {
+  const nextPrimeFrom = x => {
+    const isPrime = x => {
+      const tail = Array.from({ length: (x - 1) }, (_, i) => i + 1).slice(1)
+      return tail.every(z => x % z !== 0)
+    }
+
+    if (isPrime(x)) return x
+
+    return nextPrimeFrom(x + 1)
+  }
+
+  const sum = numbers.reduce((acc, curr) => acc + curr, 0)
+
+  return nextPrimeFrom(sum) - sum
+}
+
+module.exports = minimumNumber

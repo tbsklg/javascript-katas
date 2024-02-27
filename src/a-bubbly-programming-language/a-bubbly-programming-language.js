953c54a0ea9a9061907e534b2e5ef7e6ef71467f
Tobias Klug <tbs.klg@gmail.com>
2024-02-27 07:46:13 +0100

    Update a bubbly programming language

--- src/a-bubbly-programming-language/a-bubbly-programming-language.js
+++ src/a-bubbly-programming-language/a-bubbly-programming-language.js
@@ -1,15 +1,11 @@
-const start = (fn) => fn([])
-const end = (stack) => stack[0]
-const push = (stack) => (x) => (fn) => fn([...stack, x])
-const add = (stack) => (fn) =>
-  fn([...stack.slice(0, -2), stack.at(-1) + stack.at(-2)])
-const sub = (stack) => (fn) =>
-  fn([...stack.slice(0, -2), stack.at(-1) - stack.at(-2)])
-const mul = (stack) => (fn) =>
-  fn([...stack.slice(0, -2), stack.at(-1) * stack.at(-2)])
-// ~~ is a bitwise NOT operator, which is a shortcut for Math.floor.
+const start = fn => fn([])
+const end = stack => stack[0]
+const push = stack => x => fn => fn([x, ...stack])
+const add = ([a, b, ...stack]) => fn => fn([a + b, ...stack])
+const sub = ([a, b, ...stack]) => fn => fn([a - b, ...stack])
+const mul = ([a, b, ...stack]) => fn => fn([a * b, ...stack])
+// ~~ is a bitwise NOT operator, which is a shortcut for Math.trunc.
 // see https://stackoverflow.com/a/5971668
-const div = (stack) => (fn) =>
-  fn([...stack.slice(0, -2), ~~(stack.at(-1) / stack.at(-2))])
+const div = ([a, b, ...stack]) => fn => fn([~~(a / b), ...stack])
 
 module.exports = { start, end, push, add, sub, mul, div }

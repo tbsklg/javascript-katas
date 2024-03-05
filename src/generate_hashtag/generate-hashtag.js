64972b13f9552d8c4bb54ff291ddab2eaec9059a
Tobias Klug <tbs.klg@gmail.com>
2024-03-05 11:18:14 +0100

    Refactor hashtag generator

--- src/generate_hashtag/generate-hashtag.js
+++ src/generate_hashtag/generate-hashtag.js
@@ -1,3 +1,16 @@
+Object.assign(String.prototype, {
+  safeHead (fallback = '') {
+    return this.head() || fallback
+  },
+  head () {
+    return this.at(0)
+  },
+  tail () {
+    return this.slice(1)
+  }
+}
+)
+
 const generateHashtag = str => validate(str) && hashify(str)
 
 const validate = str => !isEmpty(str) && !greaterThan140(str)
@@ -10,10 +23,6 @@ const containsOnlyWhitespaces = str => /^\s*$/.test(str)
 
 const greaterThan140 = str => [...str].filter(x => x !== ' ').length > 139
 
-const capitalize = str => safeHead(str).toUpperCase() + tail(str)
-
-const safeHead = (str, fallback = '') => str.at(0) || fallback
-
-const tail = str => str.slice(1)
+const capitalize = str => str.safeHead().toUpperCase() + str.tail()
 
 module.exports = generateHashtag

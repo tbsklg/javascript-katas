001ba3a49336471997d54e98f00b673042fbf813
Tobias Klug <tbs.klg@gmail.com>
2024-03-25 06:34:11 +0100

    Solve follow that spy

---
+++ src/find_routes/findRoutes.js
@@ -0,0 +1,23 @@
+const findRoutes = routes => {
+  const starts = routes.map(([x, _]) => x)
+  const destinations = routes.map(([_, x]) => x)
+  const destination = destinations.find(x => !starts.includes(x))
+
+  if (!destination) return ''
+
+  const flippedRoutes = flip(routes)
+
+  const route = (current, routes) => {
+    const next = routes.find(([x, _]) => x === current)
+    if (!next) return [current]
+
+    const [from, to] = next
+    return [from, ...route(to, routes)]
+  }
+
+  return route(destination, flippedRoutes).reverse().join(', ')
+}
+
+const flip = xs => [...xs].map(([x, y]) => [y, x])
+
+module.exports = findRoutes
---
+++ test/find_routes/findRoutes.spec.js
@@ -0,0 +1,100 @@
+const { expect } = require('chai')
+
+const findRoutes = require('../../src/find_routes/findRoutes')
+
+const Test = {
+  assertEquals: (actual, expected) =>
+    expect(actual).to.equal(expected)
+}
+
+describe('Follow That Spy', function () {
+  it('Fixed', function () {
+    const routes1 = findRoutes([
+      ['MNL', 'TAG'],
+      ['CEB', 'TAC'],
+      ['TAG', 'CEB'],
+      ['TAC', 'BOR']
+    ])
+    Test.assertEquals(routes1, 'MNL, TAG, CEB, TAC, BOR')
+    const routes2 = findRoutes([
+      ['UK', 'GER'],
+      ['GER', 'BEL'],
+      ['BEL', 'CAN']
+    ])
+    Test.assertEquals(routes2, 'UK, GER, BEL, CAN')
+    const routes3 = findRoutes([
+      ['Chicago', 'Winnipeg'],
+      ['Halifax', 'Montreal'],
+      ['Montreal', 'Toronto'],
+      ['Toronto', 'Chicago'],
+      ['Winnipeg', 'Seattle']
+    ])
+    Test.assertEquals(
+      routes3,
+      'Halifax, Montreal, Toronto, Chicago, Winnipeg, Seattle'
+    )
+    const routes4 = findRoutes([
+      ['Agra', 'Tokyo'],
+      ['Seoul', 'Ljubljana'],
+      ['Ljubljana', 'Wroclaw'],
+      ['Wroclaw', 'Nashville'],
+      ['Nashville', 'Amsterdam'],
+      ['Amsterdam', 'Hull'],
+      ['Hull', 'Vancouver'],
+      ['Vancouver', 'Agra'],
+      ['Tokyo', 'Manila']
+    ])
+    Test.assertEquals(
+      routes4,
+      'Seoul, Ljubljana, Wroclaw, Nashville, Amsterdam, Hull, Vancouver, Agra, Tokyo, Manila'
+    )
+    const routes5 = findRoutes([
+      ['Calgary', 'Fargo'],
+      ['Spokane', 'Toronto'],
+      ['Winnipeg', 'Montreal'],
+      ['Toronto', 'Calgary'],
+      ['Fargo', 'Winnipeg']
+    ])
+    Test.assertEquals(
+      routes5,
+      'Spokane, Toronto, Calgary, Fargo, Winnipeg, Montreal'
+    )
+    const routes6 = findRoutes([
+      ['BRA', 'KSA'],
+      ['USA', 'BRA'],
+      ['JPN', 'PHL'],
+      ['KSA', 'UAE'],
+      ['UAE', 'JPN']
+    ])
+    Test.assertEquals(routes6, 'USA, BRA, KSA, UAE, JPN, PHL')
+    const routes7 = findRoutes([['HQ', 'SH']])
+    Test.assertEquals(routes7, 'HQ, SH')
+    const routes8 = findRoutes([
+      ['San Policarpo', 'Oras'],
+      ['Balangiga', 'Lawaan'],
+      ['Borongan', 'Maydolong'],
+      ['Jipapad', 'Maslog'],
+      ['Balangkayan', 'Llorente'],
+      ['Mercedes', 'Guiuan'],
+      ['Taft', 'Sulat'],
+      ['Sulat', 'San Julian'],
+      ['Arteche', 'San Policarpo'],
+      ['Oras', 'Dolores'],
+      ['Dolores', 'Can-avid'],
+      ['Can-avid', 'Taft'],
+      ['San Julian', 'Borongan'],
+      ['Maydolong', 'Balangkayan'],
+      ['Llorente', 'Hernani'],
+      ['Hernani', 'General MacArthur'],
+      ['General MacArthur', 'Giporlos'],
+      ['Giporlos', 'Balangiga'],
+      ['Lawaan', 'Salcedo'],
+      ['Salcedo', 'Mercedes'],
+      ['Maslog', 'Arteche']
+    ])
+    Test.assertEquals(
+      routes8,
+      'Jipapad, Maslog, Arteche, San Policarpo, Oras, Dolores, Can-avid, Taft, Sulat, San Julian, Borongan, Maydolong, Balangkayan, Llorente, Hernani, General MacArthur, Giporlos, Balangiga, Lawaan, Salcedo, Mercedes, Guiuan'
+    )
+  })
+})

a2ba4409dc974b296affc0055e030215c375849c
Tobias Klug <tbs.klg@gmail.com>
2024-03-30 06:34:11 +0100

    Solve 62c93765cef6f10030dfa92b

---
+++ src/cats_and_shelves/catsAndShelves.js
@@ -0,0 +1,9 @@
+const solution = (start, finish) => {
+  if (start === finish) return 0
+
+  if (finish - start >= 3) return 1 + solution(start + 3, finish)
+
+  return 1 + solution(start + 1, finish)
+}
+
+module.exports = solution

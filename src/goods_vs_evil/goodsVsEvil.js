ae8b0a42ce0085e44ae4e178c5888045b54e78bb
Tobias Klug <tbs.klg@gmail.com>
2024-04-09 06:34:11 +0200

    Solve 52761ee4cffbc69732000738

---
+++ src/goods_vs_evil/goodsVsEvil.js
@@ -0,0 +1,26 @@
+function goodVsEvil (good, evil) {
+  const goodsScore = sum(zipWith(multiply)(numbers(good), goodsWorth))
+  const evilsScore = sum(zipWith(multiply)(numbers(evil), evilsWorth))
+  console.error(goodsScore)
+  console.error(evilsScore)
+
+  if (goodsScore > evilsScore) return goodsWin
+  if (evilsScore > goodsScore) return evilsWin
+
+  return draw
+}
+
+const goodsWin = 'Battle Result: Good triumphs over Evil'
+const evilsWin = 'Battle Result: Evil eradicates all trace of Good'
+const draw = 'Battle Result: No victor on this battle field'
+
+const goodsWorth = [1, 2, 3, 3, 4, 10]
+const evilsWorth = [1, 2, 2, 2, 3, 5, 10]
+
+const zipWith = fn => (xs, ys) => xs.map((x, i) => ys[i] ? fn(x, ys[i]) : x)
+const multiply = (x, y) => x * y
+const sum = xs => xs.reduce((acc, curr) => acc + curr, 0)
+const numbers = xs => words(xs).map(Number)
+const words = xs => xs.split(' ')
+
+module.exports = goodVsEvil
---
+++ test/good_vs_evil/goodVsEvil.spec.js
@@ -0,0 +1,10 @@
+const { assert } = require('chai')
+const goodVsEvil = require('../../src/goods_vs_evil/goodsVsEvil.js')
+
+describe('Sample tests', () => {
+  it('Tests', () => {
+    assert.equal(goodVsEvil('1 1 1 1 1 1', '1 1 1 1 1 1 1'), 'Battle Result: Evil eradicates all trace of Good')
+    assert.equal(goodVsEvil('0 0 0 0 0 10', '0 1 1 1 1 0 0'), 'Battle Result: Good triumphs over Evil')
+    assert.equal(goodVsEvil('1 0 0 0 0 0', '1 0 0 0 0 0 0'), 'Battle Result: No victor on this battle field')
+  })
+})

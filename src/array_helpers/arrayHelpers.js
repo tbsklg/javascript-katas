da54dd3c8f3373154f72bc2304ef460576d1894e
Tobias Klug <tbs.klg@gmail.com>
2024-04-10 06:34:11 +0200

    Solve 525d50d2037b7acd6e000534

---
+++ src/array_helpers/arrayHelpers.js
@@ -0,0 +1,29 @@
+const arrayHelpers = () => {
+  /* eslint-disable no-extend-native */
+  Array.prototype.square = function () {
+    return this.map(x => x * x)
+  }
+
+  Array.prototype.cube = function () {
+    return this.map(x => x * x * x)
+  }
+
+  Array.prototype.sum = function () {
+    return this.reduce((acc, curr) => acc + curr, 0)
+  }
+
+  Array.prototype.average = function () {
+    return this.sum() / this.length
+  }
+
+  Array.prototype.even = function () {
+    return this.filter(x => x % 2 === 0)
+  }
+
+  Array.prototype.odd = function () {
+    return this.filter(x => x % 2 !== 0)
+  }
+  /* eslint-enable no-extend-native */
+}
+
+module.exports = arrayHelpers

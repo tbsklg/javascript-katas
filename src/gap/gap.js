40c9893c9d902aa8071addd1b851b25400eedffa
Tobias Klug <tbs.klg@gmail.com>
2024-04-16 08:45:08 +0200

    Solve 561e9c843a2ef5a40c0000a4

---
+++ src/gap/gap.js
@@ -0,0 +1,30 @@
+function gap (g, m, n) {
+  const findGap = (primes, current) => {
+    const next = primes.next().value
+
+    if (!next) return null
+
+    if (current + g === next) return [current, next]
+
+    return findGap(primes, next)
+  }
+
+  const ps = primes(m, n)
+  return findGap(ps, ps.next().value)
+}
+
+const isPrime = n => {
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false
+  }
+
+  return true
+}
+
+function * primes (m, n) {
+  for (let i = m; i < n; i++) {
+    if (isPrime(i)) yield i
+  }
+}
+
+module.exports = gap

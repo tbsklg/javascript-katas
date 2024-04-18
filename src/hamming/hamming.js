e5518e62d4d306624f0ef7af24abe80d6d0af154
Tobias Klug <tbs.klg@gmail.com>
2024-04-18 10:31:41 +0200

    Solve 5410c0e6a0e736cf5b000e69

---
+++ src/hamming/hamming.js
@@ -0,0 +1,14 @@
+function hamming (a, b) {
+  return zip(a, b).filter(uncurry((x, y) => x !== y)).length
+}
+
+const zip = (xs, ys) => {
+  const l = Math.min(xs.length, ys.length)
+
+  return Array.from({ length: l }, (_, i) => i)
+    .reduce((acc, curr) => [...acc, [xs[curr], ys[curr]]], [])
+}
+
+const uncurry = fn => ([x, y]) => fn(x, y)
+
+module.exports = hamming
---
+++ test/hamming/hamming.spec.js
@@ -0,0 +1,18 @@
+const chai = require('chai')
+
+const assert = chai.assert
+chai.config.truncateThreshold = 0
+
+const hamming = require('../../src/hamming/hamming')
+
+describe('Fixed Tests', () => {
+  it('Tests', () => {
+    assert.strictEqual(hamming('I like turtles', 'I like turkeys'), 3)
+    assert.strictEqual(hamming('Hello World', 'Hello World'), 0)
+    assert.strictEqual(hamming('hello world', 'hello tokyo'), 4)
+    assert.strictEqual(hamming('a man a plan a canal', 'a man a plan sobanal'), 3)
+    assert.strictEqual(hamming('hamming and cheese', 'Hamming and Cheese'), 2)
+    assert.strictEqual(hamming('espresso', 'Expresso'), 2)
+    assert.strictEqual(hamming('old father, old artificer', 'of my soul the uncreated '), 24)
+  })
+})

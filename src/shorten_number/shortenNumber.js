bb67245c3fb2fc5e7555387ea9b0333d4fb19293
Tobias Klug <tbs.klg@gmail.com>
2024-04-23 08:57:37 +0200

    Solve 56b4af8ac6167012ec00006f

---
+++ src/shorten_number/shortenNumber.js
@@ -0,0 +1,16 @@
+const shortenNumber = (suffixes, base) => {
+  return x => {
+    if (isNaN(x) || x === '') return '' + x
+
+    const power = Math.min(
+      Math.floor(getBaseLog(base, x)),
+      suffixes.length - 1
+    )
+
+    return Math.floor(x / (Math.pow(base, power))) + suffixes[power]
+  }
+}
+
+const getBaseLog = (x, y) => Math.log(y) / Math.log(x)
+
+module.exports = shortenNumber

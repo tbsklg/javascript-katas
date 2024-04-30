f890492e94e4bdfdd9ed1f27bcc89914364e229f
Tobias Klug <tbs.klg@gmail.com>
2024-04-30 07:34:11 +0200

    Solve 6411b91a5e71b915d237332d

---
+++ src/valid_parentheses/validParentheses.js
@@ -0,0 +1,20 @@
+function validParentheses (parenStr) {
+  if (parenStr === '') return true
+  if (parenStr.at(0) !== '(') return false
+
+  const validate = (parens, stack = []) => {
+    if (parens.length === 0) return stack.length === 0
+
+    const [x, ...xs] = parens
+
+    if (x === '(') return validate(xs, [')', ...stack])
+
+    if (x === ')' && (stack.at(0) || '') === ')') return validate(xs, stack.slice(1))
+
+    return false
+  }
+
+  return validate([...parenStr])
+}
+
+module.exports = validParentheses
---
+++ test/valid_parentheses/validParentheses.spec.js
@@ -0,0 +1,35 @@
+const chai = require('chai')
+const assert = chai.assert
+
+function runTest (expected, input) {
+  assert.strictEqual(
+    validParentheses(input),
+    expected,
+    `Test failed for input: "${input}"`
+  )
+}
+
+const validParentheses = require('../../src/valid_parentheses/validParentheses')
+
+describe('Sample tests', function () {
+  it('Should return true for valid parentheses', function () {
+    runTest(true, '()')
+    runTest(true, '((()))')
+    runTest(true, '()()()')
+    runTest(true, '(()())()')
+    runTest(true, '()(())((()))(())()')
+  })
+
+  it('Should return false for invalid parentheses', function () {
+    runTest(false, ')(')
+    runTest(false, '()()(')
+    runTest(false, '((())')
+    runTest(false, '())(()')
+    runTest(false, ')()')
+    runTest(false, ')')
+  })
+
+  it('Should return true for empty strings', function () {
+    runTest(true, '')
+  })
+})

829aefa8818fa61237f3e9703ccbf54c0ce7564a
Tobias Klug <tbs.klg@gmail.com>
2024-05-04 07:34:11 +0200

    Solve 6411b91a5e71b915d237332d

---
+++ src/land_perimeter/landPerimeter.js
@@ -0,0 +1,23 @@
+const landPerimeter = (arr) => prettyPrint(perimeter(landFields(arr)))
+
+const prettyPrint = (perimeter) => `Total land perimeter: ${perimeter}`
+
+const perimeter = (fields) =>
+  fields.reduce((acc, curr) => 4 - neighbours(curr, fields) + acc, 0)
+
+const neighbours = ([i1, j1], fields) =>
+  fields.filter(
+    ([i2, j2]) =>
+      (i1 === i2 && Math.abs(j1 - j2) === 1) ||
+      (j1 === j2 && Math.abs(i1 - i2) === 1),
+  ).length
+
+const landFields = (area) => fields(area, 'X')
+
+const fields = (area, field) => {
+  return area
+    .flatMap((x, i) => [...x].map((_, j) => [j, i]))
+    .filter(([j, i]) => [...area[i]][j] === field)
+}
+
+module.exports = landPerimeter
---
+++ test/land_perimeter/landPerimeter.spec.js
@@ -0,0 +1,75 @@
+const Test = {
+  assertEquals: require('chai').assert.equal,
+}
+
+const landPerimeter = require('../../src/land_perimeter/landPerimeter')
+
+describe('Testing', function () {
+  it('Basic testing', () => {
+    Test.assertEquals(
+      landPerimeter([
+        'OXOOOX',
+        'OXOXOO',
+        'XXOOOX',
+        'OXXXOO',
+        'OOXOOX',
+        'OXOOOO',
+        'OOXOOX',
+        'OOXOOO',
+        'OXOOOO',
+        'OXOOXX',
+      ]),
+      'Total land perimeter: 60',
+    )
+    Test.assertEquals(
+      landPerimeter([
+        'OXOOO',
+        'OOXXX',
+        'OXXOO',
+        'XOOOO',
+        'XOOOO',
+        'XXXOO',
+        'XOXOO',
+        'OOOXO',
+        'OXOOX',
+        'XOOOO',
+        'OOOXO',
+      ]),
+      'Total land perimeter: 52',
+    )
+    Test.assertEquals(
+      landPerimeter([
+        'XXXXXOOO',
+        'OOXOOOOO',
+        'OOOOOOXO',
+        'XXXOOOXO',
+        'OXOXXOOX',
+      ]),
+      'Total land perimeter: 40',
+    )
+    Test.assertEquals(
+      landPerimeter([
+        'XOOOXOO',
+        'OXOOOOO',
+        'XOXOXOO',
+        'OXOXXOO',
+        'OOOOOXX',
+        'OOOXOXX',
+        'XXXXOXO',
+      ]),
+      'Total land perimeter: 54',
+    )
+    Test.assertEquals(
+      landPerimeter([
+        'OOOOXO',
+        'XOXOOX',
+        'XXOXOX',
+        'XOXOOO',
+        'OOOOOO',
+        'OOOXOO',
+        'OOXXOO',
+      ]),
+      'Total land perimeter: 40',
+    )
+  })
+})

469adf12b375fe2c5c6a23ce25376bfd490bf109
Tobias Klug <tbs.klg@gmail.com>
2024-09-21 06:30:00 +0200

    Solve 551f37452ff852b7bd000139

--- src/add_binary/addBinary.js
+++ src/add_binary/addBinary.js
@@ -1,15 +1,5 @@
 function addBinary(a, b) {
-  return toBinary(a + b)
-    .map(String)
-    .join('');
+  return (a + b).toString(2);
 }
-
-const toBinary = (x) => {
-  if (x === 0) {
-    return [];
-  }
-
-  return [...toBinary(Math.floor(x / 2)), x % 2];
-};
 
 module.exports = addBinary;
